d0efc4d34b582714ed64ed2ce54346b4f9998cee
Eddie <eddiebreeg0@protonmail.com>
2022-05-13 11:21:38 +0200

    version handling

--- include/crand/version.h
+++ include/crand/version.h
@@ -1,9 +1,14 @@
+#pragma once
+
 #ifndef CRAND_VERSION
+
+#warning undefined crand version.
+#warning Please CRAND_VERSION_MAJOR, CRAND_VERSION_MINOR and CRAND_VERSION_PATCH appropriately
 
 #define CRAND_VERSION
 
-#define CRAND_VERSION_MAJOR
-#define CRAND_VERSION_MINOR
-#define CRAND_VERSION_PATCH
+#define CRAND_VERSION_MAJOR     0
+#define CRAND_VERSION_MINOR     0
+#define CRAND_VERSION_PATCH     0
 
 #endif
